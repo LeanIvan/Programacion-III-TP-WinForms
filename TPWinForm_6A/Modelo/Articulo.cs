57da602f60cf0c59f4213567fa5bc4789e6a433a
unknown <BFraanco66@gmail.com>
2024-09-05 18:37:02 -0300

    Desarrollo de la clase AccesoDatos en Modelo

--- TPWinForm_6A/Modelo/Articulo.cs
+++ TPWinForm_6A/Modelo/Articulo.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Modelo
 {
     public class Articulo
     {
-
-
-
-
-
-
 
     }
 }
